826483e7ef7a8c54cc0552134de0a2cbc5b4cdd2
ridwanullahh <123872428+ridwanullahh@users.noreply.github.com>
2023-09-26 11:35:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reacthello
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-aayxa4)
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const root = createRoot(document.getElementById('app'));
+
+root.render(
+  <StrictMode>
+    <App />
+    <Greeting/>
+  </StrictMode>
+);
